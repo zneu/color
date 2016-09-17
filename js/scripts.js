98a7e919af49242d29b0bc959d118cbf1846fa29
Akrizae <xacharyrobert@gmail.com>
2016-09-16 23:42:41 -0700

    This commit will be the first working version of the color dveloper tool. Change the body background color, the Intro and PageColors divs, and the h1, h4, and p elements using jQuery, HTML, and CSS to manipulate the page.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@ $(document).ready(function () {
   $("#PageColors form").submit(function(event) {
     var background = $("input#BgColor").val();
     var introBg = $("input#IntroColor").val();
+    var formBg = $("input#FormColor").val();
     var header1 = $("input#h1Color").val();
     var header4 = $("input#h4Color").val();
     var paragraph = $("input#ParagraphColor").val();
 
     $("body").css("background-color", background);
     $("#Intro").css("background-color", introBg);
+    $("#PageColors").css("background-color", formBg);
     $("h1").css("color", header1);
     $("h4").css("color", header4);
     $("p").css("color", paragraph);
 
     $(".OutputBg").text(background);
     $(".OutputIntro").text(introBg);
+    $(".OutputForm").text(introBg);
     $(".OutputH1").text(header1);
     $(".OutputH4").text(header4);
     $(".OutputP").text(paragraph);
